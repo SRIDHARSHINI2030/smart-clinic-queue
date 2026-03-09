a4fcd22bc214be3387f3637f9820521125c54c46
SRIDHARSHINI2030 <dharsh2030@gmail.com>
2026-03-09 11:02:44 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,45 @@
+# Smart Clinic Queue System
+
+A simple web-based queue management system for clinics built with Flask.
+
+## Features
+
+- Add patient to queue
+- Automatic token generation
+- View waiting queue
+- Call next patient
+- Reset queue
+- Display board showing current patient and waiting count
+
+## Technologies Used
+
+- Python
+- Flask
+- HTML
+- JavaScript
+- CSS
+
+## How to Run
+
+1. Install dependencies
+
+pip install -r requirements.txt
+
+2. Run the application
+
+python app.py
+
+3. Open browser
+
+http://127.0.0.1:5000
+
+## Project Structure
+
+smart-clinic-queue
+│
+├── app.py
+├── templates/index.html
+├── static/
+├── screenshots/
+├── requirements.txt
+└── README.md
---
+++ app.py
@@ -0,0 +1,75 @@
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+queue = []
+token = 1
+current_patient = None
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/add_patient', methods=['POST'])
+def add_patient():
+    global token
+    data = request.json
+    name = data.get("name")
+
+    patient = {
+        "token": token,
+        "name": name
+    }
+
+    queue.append(patient)
+    token += 1
+
+    return jsonify({
+        "message": f"{name} added with Token {patient['token']}"
+    })
+
+@app.route('/next_patient', methods=['GET'])
+def next_patient():
+    global current_patient
+
+    if queue:
+        current_patient = queue.pop(0)
+        return jsonify({"next_patient": current_patient})
+    else:
+        return jsonify({"message": "No patients in queue"})
+
+@app.route('/queue', methods=['GET'])
+def show_queue():
+    return jsonify({"queue": queue})
+
+@app.route('/reset_queue', methods=['POST'])
+def reset_queue():
+    global queue, token
+    queue = []
+    token = 1
+    return jsonify({"message": "Queue reset successfully"})
+
+@app.route('/display_board', methods=['GET'])
+def display_board():
+
+    waiting = len(queue)
+
+    if current_patient:
+        serving = f"Token {current_patient['token']} - {current_patient['name']}"
+    else:
+        serving = "No patient being served"
+
+    wait_time = waiting * 3
+
+    return jsonify({
+        "now_serving": serving,
+        "patients_waiting": waiting,
+        "estimated_wait": wait_time
+    })
+
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
